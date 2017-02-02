310655464bae4e42d0f033f7547b8832b63106e8
Michał Jezierski <ambinanitelo@gmail.com>
2017-02-02 21:18:54 +0100

    comma-spacing & object-curly-spacing
    Two new rules added: `comma-spacing` and `object-curly-spacing`. Rules are in alphabetical order right now. The `index.js` file conforms it's own rules right now.

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 module.exports = {
-    "rules": {
-        "indent": ["warn", 4, { "SwitchCase": 1 }],
-        "quotes": ["warn", "single", { "allowTemplateLiterals": true }],
-        "linebreak-style": ["warn", "unix"],
-        "semi": ["warn", "always"],
-        "eqeqeq": "warn",
-        "max-len": ["warn", 120, 4, { "ignoreUrls": true }],
-        "comma-dangle": ["warn", "never"],
-        "no-unused-vars": ["warn"],
-        "no-undef": ["warn"]
+    rules: {
+        'comma-dangle': ['warn', 'never'],
+        'comma-spacing': ['warn', {
+            'before': false,
+            'after': true
+        }],
+        'eqeqeq': 'warn',
+        'indent': ['warn', 4, { 'SwitchCase': 1 }],
+        'linebreak-style': ['warn', 'unix'],
+        'max-len': ['warn', 120, 4, { 'ignoreUrls': true }],
+        'no-undef': ['warn'],
+        'no-unused-vars': ['warn'],
+        'object-curly-spacing': ['warn', 'always'],
+        'quotes': ['warn', 'single', { 'allowTemplateLiterals': true }],
+        'semi': ['warn', 'always']
     },
-    "env": {
-        "es6": true,
-        "node": true,
-        "browser": true
+    env: {
+        'es6': true,
+        'node': true,
+        'browser': true
     },
-    "parser": "babel-eslint"
-}
+    parser: 'babel-eslint'
+};
